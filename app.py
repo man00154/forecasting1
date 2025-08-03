0c1778872bb74de46a6fe911c5460730c1ba1fa3
man00154 <149796066+man00154@users.noreply.github.com>
2025-08-03 20:45:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import glob
+import os
 from forecasting_script import run_forecasting_pipeline
 
 app = Flask(__name__)
@@ -12,4 +13,5 @@ def index():
     return f"<h1>Forecast Completed</h1><ul>{links}</ul>"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
