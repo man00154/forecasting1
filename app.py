c3f3fc5bff2b35e87ca5906b1736bd4aac8a5800
man00154 <manishsingh00154@yahoo.com>
2025-08-03 21:43:01 +0530

    Show forecast plot on web page

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask
-import glob
+from flask import Flask, render_template, send_from_directory
 import os
-from forecasting_script import run_forecasting_pipeline
+from forecasting_script import run_forecasting_pipeline  # This should save plot to static/plots/forecast.png
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    run_forecasting_pipeline()
-    files = glob.glob("static/plots/*.png")
-    links = ''.join([f'<li><a href="/{file}">{file}</a></li>' for file in files])
-    return f"<h1>Forecast Completed</h1><ul>{links}</ul>"
+    run_forecasting_pipeline()  # Generate the forecast and save the graph
+    return render_template('index.html')
+
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))  # Render assigns a port in PORT
